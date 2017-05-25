f57ad59bf8c9e32784fb0392f5fc85a912d555e9
dahlo <m.dahlo@gmail.com>
2017-05-25 11:47:18 +0200

    Initial commit.

---
+++ spotify_instance_checker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+# check if spotify is already running
+if pgrep -x "spotify" > /dev/null
+then
+	# if it is, switch focus to it
+	wmctrl -ia $(wmctrl -lp | awk -vpid=$(ps ax | grep "/usr/share/spotify/spotify$" | grep -v grep | awk '{print $1}') '$3==pid {print $1; exit}')
+else
+	# if not, start it
+	spotify &
+fi
